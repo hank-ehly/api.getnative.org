badf8fc61b8884a4b522c134ebef90723a2eea45
Hank Ehly <henry.ehly@gmail.com>
2017-05-09 13:34:26 +0900

    Remove unused seeds for now

--- db/seeders/20170226215117-followers.js
+++ db/seeders/20170226215117-followers.js
@@ -13,36 +13,7 @@ const Promise = require('bluebird');
 
 module.exports = {
     up: function(queryInterface, Sequelize) {
-        const promises = [Account.min('id'), Account.max('id'), Speaker.min('id'), Speaker.max('id')];
-
-        return Promise.all(promises).spread((minAccountId, maxAccountId, minSpeakerId, maxSpeakerId) => {
-            let followers = [];
-
-            for (let i = minAccountId; i <= maxAccountId; i++) {
-                let numFollowers = chance.integer({
-                    min: 1,
-                    max: 5
-                });
-
-                for (let j = 0; j < numFollowers; j++) {
-                    followers.push({
-                        speaker_id: chance.integer({
-                            min: minSpeakerId,
-                            max: maxSpeakerId
-                        })
-                    });
-                }
-            }
-
-            for (let i = 0; i < followers.length; i++) {
-                followers[i].account_id = chance.integer({
-                    min: minAccountId,
-                    max: maxAccountId
-                });
-            }
-
-            return queryInterface.bulkInsert('followers', followers);
-        });
+        return new Promise(r => r());
     },
 
     down: function(queryInterface, Sequelize) {
--- db/seeders/20170226223232-notifications.js
+++ db/seeders/20170226223232-notifications.js
@@ -11,35 +11,7 @@ const Promise = require('bluebird');
 
 module.exports = {
     up: function(queryInterface, Sequelize) {
-        return Promise.all([Account.min('id'), Account.max('id')]).spread((minAccountId, maxAccountId) => {
-            const notifications = [];
-
-            for (let i = minAccountId; i <= maxAccountId; i++) {
-
-                let numNotifications = chance.integer({
-                    min: 1,
-                    max: 10
-                });
-
-                for (let j = 0; j < numNotifications; j++) {
-                    notifications.push({
-                        account_id: chance.integer({
-                            min: minAccountId,
-                            max: maxAccountId
-                        }),
-                        title: chance.sentence({
-                            words: chance.integer({
-                                min: 2,
-                                max: 5
-                            })
-                        }),
-                        content: chance.paragraph()
-                    });
-                }
-            }
-
-            return queryInterface.bulkInsert('notifications', notifications);
-        });
+        return new Promise(r => r());
     },
 
     down: function(queryInterface, Sequelize) {
