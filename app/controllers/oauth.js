73e71c1ef7b2d217c44d4a9e6aa5ed2aee9aa3e1
Hank Ehly <henry.ehly@gmail.com>
2017-05-23 11:43:07 +0900

    Refactor facebook callback logic

--- app/controllers/oauth.js
+++ app/controllers/oauth.js
@@ -6,8 +6,17 @@
  */
 
 const logger = require('../../config/logger');
+const Auth   = require('../services')['Auth'];
+const config = require('../../config/application').config;
+const k      = require('../../config/keys.json');
 
-module.exports.facebookCallback = (req, res) => {
-    logger.info(req.user);
-    res.redirect('http://localhost:5555/dashboard'); // todo: make dynamic
+const moment = require('moment');
+
+module.exports.facebookCallback = (req, res, next) => {
+    const userId = req.user.get(k.Attr.Id);
+    return Auth.generateTokenForUserId(userId).then(token => {
+        const tokenExpirationDate = moment().add(1, 'hours').valueOf().toString();
+        const redirectUrl = `${config.get(k.Client.BaseURI)}/oauth/facebook?token=${token}&expires=${tokenExpirationDate}`;
+        res.redirect(redirectUrl);
+    }).catch(next);
 };
