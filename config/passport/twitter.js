9bbc5eb9f9cd8c9e13da2910538a7e9ea84d5e9b
Hank Ehly <henry.ehly@gmail.com>
2017-05-25 08:16:10 +0900

    Add example comment and ask for email from twitter oauth

--- config/passport/twitter.js
+++ config/passport/twitter.js
@@ -13,9 +13,16 @@ const TwitterStrategy = require('passport-twitter').Strategy;
 const strategy = new TwitterStrategy({
     consumerKey: config.get(k.OAuth.Twitter.ConsumerKey),
     consumerSecret: config.get(k.OAuth.Twitter.ConsumerSecret),
-    callbackURL: config.get(k.OAuth.Twitter.CallbackURL)
+    callbackURL: config.get(k.OAuth.Twitter.CallbackURL),
+    includeEmail: true
 }, (token, tokenSecret, profile, cb) => {
-    console.log(profile);
+    // let example = {
+    //     id: '705578181558796288',
+    //     displayName: 'Hank Ehly',
+    //     emails: [{value: 'henry.ehly@gmail.com'}],
+    //     photos: [{value: 'https://abs.twimg.com/sticky/default_profile_images/default_profile_normal.png'}],
+    //     provider: 'twitter'
+    // };
     cb(null, {});
 });
 
