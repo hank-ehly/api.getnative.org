a05ebbaca3a516bd02aca0f41a9c1e1a91933088
Hank Ehly <henry.ehly@gmail.com>
2017-05-26 08:59:22 +0900

    Implement User.findOrCreateForPassportProfile in twitter/facebook strategy handlers

--- config/passport/facebook.js
+++ config/passport/facebook.js
@@ -5,124 +5,21 @@
  * Created by henryehly on 2017/05/12.
  */
 
-const config = require('../application').config;
-const logger = require('../logger');
-const k = require('../keys.json');
-const db = require('../../app/models');
-const Identity = db[k.Model.Identity];
-const AuthAdapterType = db[k.Model.AuthAdapterType];
-const User = db[k.Model.User];
-const Language = db[k.Model.Language];
-const services = require('../../app/services');
-const Auth = services['Auth'];
-const Utility = services['Utility'];
+const config   = require('../application').config;
+const User     = require('../../app/models')[k.Model.User];
+const k        = require('../keys.json');
 
-const FacebookStrategy = require('passport-facebook').Strategy;
-const _ = require('lodash');
+const Strategy = require('passport-facebook').Strategy;
 
-// todo: Refactor
-// todo: Test
 // todo: (*) Make sure time-out isn't an issue
 // todo: Update user each authentication to make sure data is correct // web-hook
-const strategy = new FacebookStrategy({
+const strategy = new Strategy({
     clientID: config.get(k.OAuth.Facebook.ClientID),
     clientSecret: config.get(k.OAuth.Facebook.ClientSecret),
-    profileFields: [
-        'id',
-        'displayName',
-        'age_range',
-        'locale',
-        'photos',
-        'emails'
-    ],
+    profileFields: ['id', 'displayName', 'age_range', 'locale', 'photos', 'emails'],
     callbackURL: config.get(k.OAuth.Facebook.CallbackURL)
-}, (accessToken, refreshToken, profile, cb) => {
-    const cache = {};
-
-    // check if another user exists with the same email
-    return User.findOne({
-        where: {
-            email: _.first(profile.emails).value
-        }
-    }).then(user => {
-        // IF: exists
-        if (user) {
-            // THEN:
-            // IF: that user has a 'facebook' identity
-            return AuthAdapterType.findOne({
-                where: {
-                    name: profile.provider
-                },
-                attributes: [
-                    k.Attr.Id
-                ]
-            }).then(adapter => {
-                cache.adapter = adapter;
-
-                return Identity.findOne({
-                    where: {
-                        user_id: user.get(k.Attr.Id),
-                        auth_adapter_type_id: adapter.get(k.Attr.Id)
-                    }
-                });
-            }).then(identity => {
-                if (identity) {
-                    // THEN: get the user data and invoke the callback
-                    return User.findById(identity.get(k.Attr.UserId)).then(user => {
-                        return cb(null, user);
-                    });
-                } else {
-                    // ELSE: create a 'facebook' identity for the user and invoke the callback with the user data
-                    return Identity.create({
-                        user_id: user.get(k.Attr.Id),
-                        auth_adapter_type_id: cache.adapter.get(k.Attr.Id),
-                        auth_adapter_user_id: _.toString(profile[k.Attr.Id])
-                    }).then(() => {
-                        return cb(null, user);
-                    });
-                }
-            });
-        }
-
-        // IF: not exists
-        else {
-            // THEN: create a user record and identity record of 'facebook' type && invoke callback with user data
-            return Language.findOne({
-                where: {
-                    code: 'en'
-                }
-            }).then(language => {
-                const user = {
-                    email: _.first(profile.emails).value,
-                    default_study_language_id: language.get(k.Attr.Id)
-                };
-
-                if (profile.photos && _.gt(_.size(profile.photos), 0)) {
-                    user[k.Attr.PictureUrl] = _.first(profile.photos).value;
-                    user[k.Attr.IsSilhouettePicture] = false;
-                }
-
-                return User.create(user);
-            }).then(user => {
-                return AuthAdapterType.findOne({
-                    where: {
-                        name: profile.provider
-                    },
-                    attributes: [
-                        k.Attr.Id
-                    ]
-                }).then(adapter => {
-                    return Identity.create({
-                        user_id: user.get(k.Attr.Id),
-                        auth_adapter_type_id: adapter.get(k.Attr.Id),
-                        auth_adapter_user_id: _.toString(profile[k.Attr.Id])
-                    });
-                }).then(() => {
-                    return cb(null, user);
-                });
-            });
-        }
-    });
+}, async (accessToken, refreshToken, profile, callback) => {
+    return callback(null, await User.findOrCreateFromPassportProfile(profile));
 });
 
 module.exports = strategy;
--- config/passport/twitter.js
+++ config/passport/twitter.js
@@ -5,25 +5,19 @@
  * Created by henryehly on 2017/05/24.
  */
 
-const config          = require('../application').config;
-const k               = require('../keys.json');
+const config   = require('../application').config;
+const User     = require('../../app/models')[k.Model.User];
+const k        = require('../keys.json');
 
-const TwitterStrategy = require('passport-twitter').Strategy;
+const Strategy = require('passport-twitter').Strategy;
 
-const strategy = new TwitterStrategy({
+const strategy = new Strategy({
     consumerKey: config.get(k.OAuth.Twitter.ConsumerKey),
     consumerSecret: config.get(k.OAuth.Twitter.ConsumerSecret),
     callbackURL: config.get(k.OAuth.Twitter.CallbackURL),
     includeEmail: true
-}, (token, tokenSecret, profile, cb) => {
-    // let example = {
-    //     id: '705578181558796288',
-    //     displayName: 'Hank Ehly',
-    //     emails: [{value: 'henry.ehly@gmail.com'}],
-    //     photos: [{value: 'https://abs.twimg.com/sticky/default_profile_images/default_profile_normal.png'}],
-    //     provider: 'twitter'
-    // };
-    cb(null, {});
+}, async (token, tokenSecret, profile, callback) => {
+    return callback(null, await User.findOrCreateFromPassportProfile(profile));
 });
 
 module.exports = strategy;
