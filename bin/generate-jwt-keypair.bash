9ad8bb0b1cdcfad3b0e4e15dd437fa6f19a14f6e
Hank Ehly <henry.ehly@gmail.com>
2017-05-28 11:03:54 +0900

    Create script for generating jwt-keypair

---
+++ bin/generate-jwt-keypair.bash
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+BASENAME=`basename $0`
+TMPFILE=`mktemp -t ${BASENAME}` || exit 1
+
+echo '{"privateKey": "' >> ${TMPFILE}
+
+ssh-keygen -t rsa -N "" -f id_rsa -q
+cat id_rsa | sed 's/$/\\\n/g' | tr -d '\n' >> ${TMPFILE}
+
+echo '", "publicKey": "' >> ${TMPFILE}
+
+ssh-keygen -f id_rsa.pub -e -m pem > id_rsa.pem
+cat id_rsa.pem | sed 's/$/\\n/g' | tr -d '\n' >> ${TMPFILE}
+
+echo '"}' >> ${TMPFILE}
+
+cat ${TMPFILE} | tr -d '\n' > config/secrets/jwt-keypair.json
+
+rm -f ./id_rsa*
+rm -f ${TMPFILE}
