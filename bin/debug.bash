b81152d598b751b907f150c419191bd96750cd1c
Hank Ehly <henry.ehly@gmail.com>
2017-06-07 07:28:28 +0900

    Update node verson in debug bash script

--- bin/debug.bash
+++ bin/debug.bash
@@ -4,4 +4,4 @@ set -e
 
 NODE_ENV="development" \
 GOOGLE_APPLICATION_CREDENTIALS="config/secrets/gcloud-credentials.json" \
-/Users/henryehly/.nvm/versions/node/v7.10.0/bin/node --debug-brk=5859 index.js
+/Users/henryehly/.nvm/versions/node/v8.0.0/bin/node --debug-brk=5859 index.js
