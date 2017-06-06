641a782073f77f000c3d95eb9d8db47f8076bcb6
Hank Ehly <henry.ehly@gmail.com>
2017-06-07 07:29:13 +0900

    Use user environment node version in bash script

--- bin/debug.bash
+++ bin/debug.bash
@@ -4,4 +4,4 @@ set -e
 
 NODE_ENV="development" \
 GOOGLE_APPLICATION_CREDENTIALS="config/secrets/gcloud-credentials.json" \
-/Users/henryehly/.nvm/versions/node/v8.0.0/bin/node --debug-brk=5859 index.js
+/usr/bin/env node --debug-brk=5859 index.js
