a597e8c280e5a93c6cd694987a6f9756591eb414
Hank Ehly <henry.ehly@gmail.com>
2017-07-14 20:02:27 +0900

    Use PATCH

--- spec/controllers/collocation-occurrences/update.spec.js
+++ spec/controllers/collocation-occurrences/update.spec.js
@@ -48,56 +48,56 @@ describe('POST /collocation_occurrences/:id', function() {
 
     describe('failure', function() {
         it(`should respond with 401 Unauthorized if the request does not contain an 'authorization' header`, function() {
-            return request(server).post(`/collocation_occurrences/${collocation.get(k.Attr.Id)}`).send(validBody).expect(401);
+            return request(server).patch(`/collocation_occurrences/${collocation.get(k.Attr.Id)}`).send(validBody).expect(401);
         });
 
         it('should respond with 400 Bad Request if the :id parameter is not a number', function() {
-            return request(server).post(`/collocation_occurrences/not_a_number`).set(k.Header.Authorization, authorization).send(validBody).expect(400);
+            return request(server).patch(`/collocation_occurrences/not_a_number`).set(k.Header.Authorization, authorization).send(validBody).expect(400);
         });
 
         it('should respond with 400 Bad Request if the :id parameter is 0', function() {
-            return request(server).post(`/collocation_occurrences/0`).set(k.Header.Authorization, authorization).send(validBody).expect(400);
+            return request(server).patch(`/collocation_occurrences/0`).set(k.Header.Authorization, authorization).send(validBody).expect(400);
         });
 
         it('should respond with 404 Not Found if no CollocationOccurrence record for the provided :id exists', function() {
-            return request(server).post(`/collocation_occurrences/${Math.pow(10, 6)}`).set(k.Header.Authorization, authorization).send(validBody).expect(404);
+            return request(server).patch(`/collocation_occurrences/${Math.pow(10, 6)}`).set(k.Header.Authorization, authorization).send(validBody).expect(404);
         });
 
         it('should respond with 400 Bad Request if the "ipa_spelling" parameter is an empty string', function() {
-            return request(server).post(`/collocation_occurrences/${collocation.get(k.Attr.Id)}`).set(k.Header.Authorization, authorization).send({ipa_spelling: _.stubString()}).expect(400);
+            return request(server).patch(`/collocation_occurrences/${collocation.get(k.Attr.Id)}`).set(k.Header.Authorization, authorization).send({ipa_spelling: _.stubString()}).expect(400);
         });
 
         it('should respond with 400 Bad Request if the "ipa_spelling" parameter is not a string', function() {
-            return request(server).post(`/collocation_occurrences/${collocation.get(k.Attr.Id)}`).set(k.Header.Authorization, authorization).send({ipa_spelling: _.stubObject()}).expect(400);
+            return request(server).patch(`/collocation_occurrences/${collocation.get(k.Attr.Id)}`).set(k.Header.Authorization, authorization).send({ipa_spelling: _.stubObject()}).expect(400);
         });
 
         it('should respond with 400 Bad Request if the "ipa_spelling" parameter is longer than 100 chars', function() {
             const longString = _.times(101, _.constant('x')).join('');
-            return request(server).post(`/collocation_occurrences/${collocation.get(k.Attr.Id)}`).set(k.Header.Authorization, authorization).send({ipa_spelling: longString}).expect(400);
+            return request(server).patch(`/collocation_occurrences/${collocation.get(k.Attr.Id)}`).set(k.Header.Authorization, authorization).send({ipa_spelling: longString}).expect(400);
         });
 
         it('should respond with 304 if the request body is empty', function() {
-            return request(server).post(`/collocation_occurrences/${collocation.get(k.Attr.Id)}`).set(k.Header.Authorization, authorization).send({}).expect(304);
+            return request(server).patch(`/collocation_occurrences/${collocation.get(k.Attr.Id)}`).set(k.Header.Authorization, authorization).send({}).expect(304);
         });
     });
 
     describe('success', function() {
         it('should respond with an X-GN-Auth-Token header', async function() {
-            const response = await request(server).post(`/collocation_occurrences/${collocation.get(k.Attr.Id)}`).set(k.Header.Authorization, authorization).send(validBody);
+            const response = await request(server).patch(`/collocation_occurrences/${collocation.get(k.Attr.Id)}`).set(k.Header.Authorization, authorization).send(validBody);
             assert(response.header[k.Header.AuthToken].length > 0);
         });
 
         it('should respond with an X-GN-Auth-Expire header containing a valid timestamp value', async function() {
-            const response = await request(server).post(`/collocation_occurrences/${collocation.get(k.Attr.Id)}`).set(k.Header.Authorization, authorization).send(validBody);
+            const response = await request(server).patch(`/collocation_occurrences/${collocation.get(k.Attr.Id)}`).set(k.Header.Authorization, authorization).send(validBody);
             assert(SpecUtil.isParsableTimestamp(+response.header[k.Header.AuthExpire]));
         });
 
         it('should return a 204 response for a valid request', function() {
-            return request(server).post(`/collocation_occurrences/${collocation.get(k.Attr.Id)}`).set(k.Header.Authorization, authorization).send(validBody).expect(204);
+            return request(server).patch(`/collocation_occurrences/${collocation.get(k.Attr.Id)}`).set(k.Header.Authorization, authorization).send(validBody).expect(204);
         });
 
         it('should update the ipa_spelling property of the CollocationOccurrence record', async function() {
-            await request(server).post(`/collocation_occurrences/${collocation.get(k.Attr.Id)}`).set(k.Header.Authorization, authorization).send(validBody);
+            await request(server).patch(`/collocation_occurrences/${collocation.get(k.Attr.Id)}`).set(k.Header.Authorization, authorization).send(validBody);
             const co = await db[k.Model.CollocationOccurrence].find({where: {ipa_spelling: validBody.ipa_spelling}});
             assert(co);
         });
--- spec/controllers/usage-examples/update.spec.js
+++ spec/controllers/usage-examples/update.spec.js
@@ -48,56 +48,56 @@ describe('POST /usage_examples/:id', function() {
 
     describe('failure', function() {
         it(`should respond with 401 Unauthorized if the request does not contain an 'authorization' header`, function() {
-            return request(server).post(`/usage_examples/${usageExample.get(k.Attr.Id)}`).send(validBody).expect(401);
+            return request(server).patch(`/usage_examples/${usageExample.get(k.Attr.Id)}`).send(validBody).expect(401);
         });
 
         it('should respond with 400 Bad Request if the :id parameter is not a number', function() {
-            return request(server).post(`/usage_examples/not_a_number`).set(k.Header.Authorization, authorization).send(validBody).expect(400);
+            return request(server).patch(`/usage_examples/not_a_number`).set(k.Header.Authorization, authorization).send(validBody).expect(400);
         });
 
         it('should respond with 400 Bad Request if the :id parameter is 0', function() {
-            return request(server).post(`/usage_examples/0`).set(k.Header.Authorization, authorization).send(validBody).expect(400);
+            return request(server).patch(`/usage_examples/0`).set(k.Header.Authorization, authorization).send(validBody).expect(400);
         });
 
         it('should respond with 404 Not Found if no UsageExample record for the provided :id exists', function() {
-            return request(server).post(`/usage_examples/${Math.pow(10, 6)}`).set(k.Header.Authorization, authorization).send(validBody).expect(404);
+            return request(server).patch(`/usage_examples/${Math.pow(10, 6)}`).set(k.Header.Authorization, authorization).send(validBody).expect(404);
         });
 
         it('should respond with 400 Bad Request if the "text" parameter is not a string', function() {
-            return request(server).post(`/usage_examples/${usageExample.get(k.Attr.Id)}`).set(k.Header.Authorization, authorization).send({text: _.stubObject()}).expect(400);
+            return request(server).patch(`/usage_examples/${usageExample.get(k.Attr.Id)}`).set(k.Header.Authorization, authorization).send({text: _.stubObject()}).expect(400);
         });
 
         it('should respond with 400 Bad Request if the "text" parameter is an empty string', function() {
-            return request(server).post(`/usage_examples/${usageExample.get(k.Attr.Id)}`).set(k.Header.Authorization, authorization).send({text: _.stubString()}).expect(400);
+            return request(server).patch(`/usage_examples/${usageExample.get(k.Attr.Id)}`).set(k.Header.Authorization, authorization).send({text: _.stubString()}).expect(400);
         });
 
         it('should respond with 400 Bad Request if the "text" parameter is longer than 200 chars', function() {
             const longString = _.times(201, _.constant('x')).join('');
-            return request(server).post(`/usage_examples/${usageExample.get(k.Attr.Id)}`).set(k.Header.Authorization, authorization).send({text: longString}).expect(400);
+            return request(server).patch(`/usage_examples/${usageExample.get(k.Attr.Id)}`).set(k.Header.Authorization, authorization).send({text: longString}).expect(400);
         });
     });
 
     describe('success', function() {
         it('should respond with an X-GN-Auth-Token header', async function() {
-            const response = await request(server).post(`/usage_examples/${usageExample.get(k.Attr.Id)}`).set(k.Header.Authorization, authorization).send(validBody);
+            const response = await request(server).patch(`/usage_examples/${usageExample.get(k.Attr.Id)}`).set(k.Header.Authorization, authorization).send(validBody);
             assert(response.header[k.Header.AuthToken].length > 0);
         });
 
         it('should respond with an X-GN-Auth-Expire header containing a valid timestamp value', async function() {
-            const response = await request(server).post(`/usage_examples/${usageExample.get(k.Attr.Id)}`).set(k.Header.Authorization, authorization).send(validBody);
+            const response = await request(server).patch(`/usage_examples/${usageExample.get(k.Attr.Id)}`).set(k.Header.Authorization, authorization).send(validBody);
             assert(SpecUtil.isParsableTimestamp(+response.header[k.Header.AuthExpire]));
         });
 
         it('should return a 204 response for a valid request', function() {
-            return request(server).post(`/usage_examples/${usageExample.get(k.Attr.Id)}`).set(k.Header.Authorization, authorization).send(validBody).expect(204);
+            return request(server).patch(`/usage_examples/${usageExample.get(k.Attr.Id)}`).set(k.Header.Authorization, authorization).send(validBody).expect(204);
         });
 
         it('should respond with 304 if the request body is empty', function() {
-            return request(server).post(`/usage_examples/${usageExample.get(k.Attr.Id)}`).set(k.Header.Authorization, authorization).send({}).expect(304);
+            return request(server).patch(`/usage_examples/${usageExample.get(k.Attr.Id)}`).set(k.Header.Authorization, authorization).send({}).expect(304);
         });
 
         it('should update the "text" property of the UsageExample record', async function() {
-            await request(server).post(`/usage_examples/${usageExample.get(k.Attr.Id)}`).set(k.Header.Authorization, authorization).send(validBody);
+            await request(server).patch(`/usage_examples/${usageExample.get(k.Attr.Id)}`).set(k.Header.Authorization, authorization).send(validBody);
             const example = await db[k.Model.UsageExample].find({where: {text: validBody.text}});
             assert(example);
         });
