01c5fc9cf8cf2c7d7b28d3af2b3eae5d9bb01d4b
Hank Ehly <henry.ehly@gmail.com>
2017-07-26 16:10:08 +0900

    Refactor sessions.create

--- app/controllers/sessions.js
+++ app/controllers/sessions.js
@@ -13,12 +13,12 @@ const db = require('../models');
 const Credential = db[k.Model.Credential];
 const User = db[k.Model.User];
 
-// todo: handle a user with non-local identity trying to create session
 module.exports.create = async (req, res, next) => {
-    let user, credential, token;
+    let user, credential;
 
     try {
         user = await User.find({
+            rejectOnEmpty: true,
             where: {email: req.body[k.Attr.Email]},
             attributes: [
                 k.Attr.Id, k.Attr.Email, k.Attr.BrowserNotificationsEnabled, k.Attr.EmailNotificationsEnabled, k.Attr.EmailVerified,
@@ -26,43 +26,38 @@ module.exports.create = async (req, res, next) => {
             ]
         });
     } catch (e) {
+        if (e instanceof db.sequelize.EmptyResultError) {
+            res.status(404);
+            return next(new GetNativeError(k.Error.UserNamePasswordIncorrect));
+        }
         return next(e);
-    }
-
-    if (!user) {
-        res.status(404);
-        return next(new GetNativeError(k.Error.UserNamePasswordIncorrect));
     }
 
     try {
         credential = await Credential.find({
+            rejectOnEmpty: true,
             where: {user_id: user.get(k.Attr.Id)},
             attributes: [k.Attr.Password]
         });
     } catch (e) {
+        if (e instanceof db.sequelize.EmptyResultError) {
+            res.status(404);
+            return next(new GetNativeError(k.Error.UserNamePasswordIncorrect));
+        }
         return next(e);
     }
 
-    if (!credential || !Auth.verifyPassword(credential.get(k.Attr.Password), req.body[k.Attr.Password])) {
+    if (!Auth.verifyPassword(credential.get(k.Attr.Password), req.body[k.Attr.Password])) {
         res.status(404);
         return next(new GetNativeError(k.Error.UserNamePasswordIncorrect));
     }
 
     try {
-        token = await Auth.generateTokenForUserId(user.get(k.Attr.Id));
+        const token = await Auth.generateTokenForUserId(user.get(k.Attr.Id));
+        Auth.setAuthHeadersOnResponseWithToken(res, token);
     } catch (e) {
         return next(e);
     }
 
-    if (!token) {
-        throw new ReferenceError('variable token is undefined');
-    }
-
-    Auth.setAuthHeadersOnResponseWithToken(res, token);
-
-    user = user.get({
-        plain: true
-    });
-
-    return res.status(201).send(user);
+    return res.status(201).send(user.get({plain: true}));
 };
