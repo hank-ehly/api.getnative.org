869b1a86d930fb564e20b1a3388999d6c9dcab55
Hank Ehly <henry.ehly@gmail.com>
2017-08-10 19:17:42 +0900

    Install ruby with rbenv

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -7,8 +7,8 @@ fi
 
 if [ "${1}" == "develop" ]; then
 		git clone git@github.com:hank-ehly/devops.getnativelearning.com.git
-		cd devops.getnativelearning.com/deploy && bundle install --jobs 4 --retry 3 --path vendor/bundle && bundle exec cap server:staging deploy
+		cd devops.getnativelearning.com/deploy && rbenv install --verbose && gem install bundler && bundle install --jobs 4 --retry 3 --path vendor/bundle && bundle exec cap server:staging deploy
 elif [ "${1}" == "master" ]; then
 		git clone git@github.com:hank-ehly/devops.getnativelearning.com.git
-		cd devops.getnativelearning.com/deploy && bundle install --jobs 4 --retry 3 --path vendor/bundle && bundle exec cap server:production deploy
+		cd devops.getnativelearning.com/deploy && rbenv install --verbose && gem install bundler && bundle install --jobs 4 --retry 3 --path vendor/bundle && bundle exec cap server:production deploy
 fi
