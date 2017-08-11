de7550744c94a76431892c43731b6becdb9b8e95
Hank Ehly <henry.ehly@gmail.com>
2017-08-11 13:02:52 +0900

    Format code

--- .circleci/nvm.sh
+++ .circleci/nvm.sh
@@ -4,7 +4,7 @@ export NVM_DIR="$HOME/.nvm"
 
 mv .nvmrc .nvmrc.bak \
     && curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash \
-    && source $NVM_DIR/nvm.sh \
+    && source ${NVM_DIR}/nvm.sh \
     && mv .nvmrc.bak .nvmrc \
     && nvm install 8.3.0 \
     && npm install -g npm
