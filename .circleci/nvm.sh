002f8d0396ab96deba42f5100fdd7983f1474509
Hank Ehly <henry.ehly@gmail.com>
2017-08-18 15:58:15 +0900

    Update node version

--- .circleci/nvm.sh
+++ .circleci/nvm.sh
@@ -6,5 +6,5 @@ mv .nvmrc .nvmrc.bak \
     && curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash \
     && source ${NVM_DIR}/nvm.sh \
     && mv .nvmrc.bak .nvmrc \
-    && nvm install 8.3.0 \
+    && nvm install 8.4.0 \
     && npm install -g npm
