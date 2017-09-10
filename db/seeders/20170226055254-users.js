332e1b3847dec8120c9b00bd249b61497f287a69
Hank Ehly <henry.ehly@gmail.com>
2017-09-11 07:47:30 +0900

    Synchronize DB values

--- db/seeders/20170226055254-users.js
+++ db/seeders/20170226055254-users.js
@@ -17,25 +17,28 @@ module.exports = {
         const count = process.env['NODE_ENV'] === k.Env.Test ? 5 : 500;
 
         return Language.findAll({attributes: [k.Attr.Id, k.Attr.Code]}).then(languages => {
-            const records = _.times(count, () => {
-                return {
+            const records = [];
+            for (let i = 0; i < count; i++) {
+                let isSilhouettePicture = chance.bool();
+
+                records.push({
                     browser_notifications_enabled: chance.bool(),
                     default_study_language_id: _.sample(languages).get(k.Attr.Id),
                     interface_language_id: _.sample(languages).get(k.Attr.Id),
                     email: chance.email(),
                     email_notifications_enabled: chance.bool(),
                     email_verified: chance.bool({likelihood: 80}),
-                    is_silhouette_picture: chance.bool(),
+                    is_silhouette_picture: isSilhouettePicture,
                     name: chance.name(),
-                    picture_url: 'https://dummyimage.com/100x100.png/5fa2dd/ffffff'
-                }
-            });
+                    picture_url: isSilhouettePicture ? '' : 'https://dummyimage.com/100x100.png/5fa2dd/ffffff'
+                });
+            }
 
             /* For testing purposes */
             const englishLanguageId = _.find(languages, {code: 'en'}).get(k.Attr.Id);
             _.nth(records, 0)[k.Attr.Email] = 'test@email.com';
-            _.nth(records, 1)[k.Attr.Email] = 'admin@email.com';
             _.nth(records, 0).interface_language_id = englishLanguageId;
+            _.nth(records, 1)[k.Attr.Email] = 'admin@email.com';
             _.nth(records, 1).interface_language_id = englishLanguageId;
 
             return queryInterface.bulkInsert('users', records);
