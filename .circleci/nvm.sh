83c14403dcca7034aa71a5f577fafb819276a96f
Hank Ehly <henry.ehly@gmail.com>
2017-10-12 15:16:59 -0500

    Use custom nvm install script

---
+++ .circleci/nvm.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+export NVM_DIR="$HOME/.nvm"
+
+curl -o- curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.5/install.sh | bash \
+    && source ${NVM_DIR}/nvm.sh \
+    && nvm install  8.5.0 \
+    && npm install -g npm \
+    && npm -v
