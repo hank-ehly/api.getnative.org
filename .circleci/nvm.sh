0608e6bd4138110c851bf68ecb42c2a03fdebcf2
Hank Ehly <henry.ehly@gmail.com>
2017-10-12 15:22:43 -0500

    Fix curl statement

--- .circleci/nvm.sh
+++ .circleci/nvm.sh
@@ -2,7 +2,7 @@
 
 export NVM_DIR="$HOME/.nvm"
 
-curl -o- curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.5/install.sh | bash \
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.5/install.sh | bash \
     && source ${NVM_DIR}/nvm.sh \
     && nvm install  8.5.0 \
     && npm install -g npm \
