ea9d3ec625198f27a1811f97a2a177251f081d3a
Hank Ehly <henry.ehly@gmail.com>
2017-10-12 14:18:39 -0500

    Update production config

--- config/environments/staging.js
+++ config/environments/staging.js
@@ -6,7 +6,7 @@
  */
 
 const k = require('../keys.json');
-const oauthSecrets = require('../secrets/oauth.json');
+const OAuthConfig = require('../secrets/oauth.json');
 
 const config = {};
 
@@ -21,14 +21,14 @@ config[k.EmailAddress.NoReply] = 'noreply@stg.getnativelearning.com';
 
 config[k.GoogleCloud.StorageBucketName] = 'stg.getnativelearning.com';
 
-config[k.OAuth.Facebook.ClientID] = oauthSecrets.staging.facebook.clientId;
-config[k.OAuth.Facebook.ClientSecret] = oauthSecrets.staging.facebook.clientSecret;
+config[k.OAuth.Facebook.ClientID] = OAuthConfig.staging.facebook.clientId;
+config[k.OAuth.Facebook.ClientSecret] = OAuthConfig.staging.facebook.clientSecret;
 config[k.OAuth.Facebook.CallbackURL] = 'https://api.stg.getnativelearning.com/oauth/facebook/callback';
-config[k.OAuth.Twitter.ConsumerKey] = oauthSecrets.staging.twitter.consumerKey;
-config[k.OAuth.Twitter.ConsumerSecret] = oauthSecrets.staging.twitter.consumerSecret;
+config[k.OAuth.Twitter.ConsumerKey] = OAuthConfig.staging.twitter.consumerKey;
+config[k.OAuth.Twitter.ConsumerSecret] = OAuthConfig.staging.twitter.consumerSecret;
 config[k.OAuth.Twitter.CallbackURL] = 'https://api.stg.getnativelearning.com/oauth/twitter/callback';
-config[k.OAuth.Google.ClientID] = oauthSecrets.staging.google.clientId;
-config[k.OAuth.Google.ClientSecret] = oauthSecrets.staging.google.clientSecret;
+config[k.OAuth.Google.ClientID] = OAuthConfig.staging.google.clientId;
+config[k.OAuth.Google.ClientSecret] = OAuthConfig.staging.google.clientSecret;
 config[k.OAuth.Google.CallbackURL] = 'https://api.stg.getnativelearning.com/oauth/google/callback';
 
 config[k.SMTP.Port] = 25;
