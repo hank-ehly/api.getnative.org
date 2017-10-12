8239bc5baf6042b1769528e277c1c4a443c93d4c
Hank Ehly <henry.ehly@gmail.com>
2017-10-12 16:02:33 -0500

    Revert to newest nvm version

--- .circleci/nvm.sh
+++ .circleci/nvm.sh
@@ -2,7 +2,8 @@
 
 export NVM_DIR="$HOME/.nvm"
 
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash \
-    && nvm install  8.5.0 \
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.5/install.sh | bash \
+    && source ${NVM_DIR}/nvm.sh \
+    && nvm install 8.5.0 \
     && npm install -g npm \
     && npm -v
