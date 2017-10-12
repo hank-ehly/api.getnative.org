32ceb1d649b858e8e9c69a0dc0e297e0a2db330f
Hank Ehly <henry.ehly@gmail.com>
2017-10-12 15:37:47 -0500

    Try using older version of nvm

--- .circleci/nvm.sh
+++ .circleci/nvm.sh
@@ -2,7 +2,7 @@
 
 export NVM_DIR="$HOME/.nvm"
 
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.5/install.sh | bash \
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash \
     && source ${NVM_DIR}/nvm.sh \
     && nvm install  8.5.0 \
     && npm install -g npm \
