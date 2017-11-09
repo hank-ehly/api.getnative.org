d2f816c23712d90c3395fab3f8cfeb440105dd90
Hank Ehly <henry.ehly@gmail.com>
2017-11-09 09:39:07 +0900

    Update node version

--- .circleci/nvm.sh
+++ .circleci/nvm.sh
@@ -4,6 +4,6 @@ export NVM_DIR="$HOME/.nvm"
 
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.5/install.sh | bash \
     && source ${NVM_DIR}/nvm.sh \
-    && nvm install 8.5.0 \
+    && nvm install 8.9.1 \
     && npm install -g npm \
     && npm -v
