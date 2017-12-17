2cb616e994eb146488d167c304bd368788aa59ce
Hank Ehly <henry.ehly@gmail.com>
2017-12-17 12:14:26 +0900

    Remove global hooks from model spec

--- spec/models/Video/video.js
+++ spec/models/Video/video.js
@@ -20,14 +20,6 @@ describe('Video', function() {
     let server = null;
 
     before(function() {
-        return SpecUtil.startMailServer();
-    });
-
-    after(function() {
-        return SpecUtil.stopMailServer();
-    });
-
-    before(function() {
         this.timeout(SpecUtil.defaultTimeout);
         return SpecUtil.seedAll();
     });
