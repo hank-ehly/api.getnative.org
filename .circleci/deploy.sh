2b14f16c281397fbc6324c193ee19e0dda5ea57a
Hank Ehly <henry.ehly@gmail.com>
2018-03-11 11:04:25 +0900

    Modify deploy.sh to use local capistrano config

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -12,5 +12,4 @@ fi
 
 [[ ${1} = "master" ]] && stage="production" || stage="staging"
 
-git clone git@github.com:hank-ehly/devops.getnativelearning.com.git
-cd devops.getnativelearning.com/deploy && bundle install --jobs 4 --retry 3 --path vendor/bundle && bundle exec cap api:${stage} deploy
+bundle install --jobs 4 --retry 3 --deployment && bundle exec cap ${stage} deploy
