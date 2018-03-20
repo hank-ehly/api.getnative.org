cd58e59ad331ddcb89fb8470bf1d6fe51900cf22
Hank Ehly <henry.ehly@gmail.com>
2018-03-20 17:55:31 +0900

    Fix gcloud credentials path

--- config/environments/production.js
+++ config/environments/production.js
@@ -23,7 +23,7 @@ config[k.EmailAddress.Contact] = 'contact@getnative.org';
 config[k.EmailAddress.NoReply] = 'noreply@getnative.org';
 
 config[k.GoogleCloud.APIKey] = require('../secrets/google_api_keys.json').production;
-config[k.GoogleCloud.KeyFilename] = path.resolve(__dirname, 'secrets', 'gcloud-credentials.json');
+config[k.GoogleCloud.KeyFilename] = path.resolve(__dirname, '..', 'secrets', 'gcloud-credentials.json');
 config[k.GoogleCloud.StorageBucketName] = 'getnative.org';
 
 config[k.MailChimp.APIKey] = mailchimpConfig.apiKey;
